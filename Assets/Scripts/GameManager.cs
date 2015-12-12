cb4fe992fcb37b6425a6b26f09b3e35bdd6e6b82
hmanfroi <henriquemanfroi@MacBook-Pro-de-Henrique.local>
2015-12-12 17:59:49 -0200

    Score And Queue

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,93 @@ using System.Collections.Generic;
 public class GameManager : MonoBehaviour {
 
 	public int score;
-	public GameObject[] lista;
+
+	public Queue<GameObject>[] filaRitmos;
+
+	public GameObject ritmoModel;
+
+	public GameObject[] area;
+
+	public int comboCount = 0;
+
+	public Vector3[] spawnPosition;
+
+	public float perfeito;
+
+	public float bom;
+
+	public int countPerfeito;
+
+	public int countBom;
+
+	public int countErros;
+
 	// Use this for initialization
 	void Start () {
-	
+		InvokeRepeating("GameObjectSpawn", 0.0f, 2.0f); // update at 15 fps
+		countPerfeito = 0;
+		countBom = 0;
+		countErros = 0;
+
+		filaRitmos = new Queue<GameObject>[2];
+		filaRitmos[0] = new Queue<GameObject>();
+		filaRitmos[1] = new Queue<GameObject>();
+	}
+
+	void GameObjectSpawn(){
+		
+		int res = UnityEngine.Random.Range (0, 2);
+		GameObject obj =(GameObject) Instantiate (ritmoModel, spawnPosition[res], Quaternion.identity);
+		filaRitmos [res].Enqueue (obj);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		/*
+		for(int i = 0; i < Input.touchCount; i++)
+		{
+			Touch myTouch = Input.GetTouch(i);
+			//Do something with the touches
+			if (myTouch.position.x <= Screen.width / 2) {
+				Destroy (filaRitmo[0]);
+			}
+		}
+		*/
+		if (Input.GetMouseButtonDown(0)) {
+			int ind = 1;
+			if (Input.mousePosition.x <= Screen.width / 2) { //Lado Esquerdo
+				ind = 0;
+			}
+			if (filaRitmos[ind].Count > 0) {
+				GameObject ok = (GameObject)filaRitmos[ind].Dequeue ();
+				UpdateScore (ok, area[ind]);
+				Destroy (ok);
+			}
+		}
+		//Button Pause?
+
+		//Lado Esquerdo
+
+
+		//Lado Direito
+	}
+
+	void UpdateScore(GameObject obj, GameObject area){
+		
+		if (Mathf.Abs (area.transform.position.y - obj.transform.position.y) < perfeito) {
+			score += 10;
+			comboCount++;
+			countPerfeito++;
+		} else if (Mathf.Abs (area.transform.position.y - obj.transform.position.y) < bom) {
+			score += 8;
+			comboCount++;
+			countBom++;
+		} else {
+			comboCount = 0;
+			countErros++;
+			//Fail
+		}
+		
 	}
 }
---
+++ Assets/Scripts/RenderQueue.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderQueue : MonoBehaviour {
+
+	public int renderOrder;
+	// Use this for initialization
+	void Start () {
+		Renderer[] renders = GetComponentsInChildren<Renderer> ();
+
+		for (int i = 0; i < renders.Length; i++) {
+			renders [i].material.renderQueue = renderOrder;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
