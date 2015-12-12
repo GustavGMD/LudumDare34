12abf8f48f6ef5e070a94977c85c5bbc5585d649
hmanfroi <henriquemanfroi@MacBook-Pro-de-Henrique.local>
2015-12-12 11:46:22 -0200

    Ritmo Movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public int score;
+	public GameObject[] lista;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
