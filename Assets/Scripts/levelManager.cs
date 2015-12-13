b41e9700d24183ea79d35440aa30aa67fdb7baca
hmanfroi <henriquemanfroi@MacBook-Pro-de-Henrique.local>
2015-12-13 09:20:48 -0200

    Select Level

--- Assets/Scripts/levelManager.cs
+++ Assets/Scripts/levelManager.cs
@@ -9,9 +9,17 @@ public class levelManager : MonoBehaviour {
 
 	public int totalLevels;
 	public int level;
+
+	public int plantSelected;
+
+	public Sprite[] sprites; 
+
+	public Button[] selectPlant;
+
+	public Sprite spritePlant;
 	// Use this for initialization
 	void Start () {
-
+		plantSelected = 0;
 		for (int i = 0; i < levelButtons.Length; i++) {
 			int num = i;
 			levelButtons [i].GetComponentInChildren<Text> ().text = " "+(i+1);
@@ -21,6 +29,12 @@ public class levelManager : MonoBehaviour {
 				transicao();
 			});
 		}
+		for (int i = 0; i < selectPlant.Length; i++) {
+			selectPlant [i].onClick.AddListener (delegate {
+				changePlant (i);
+			});
+		}
+
 		/*for (int i = 0; i < levels; i++) {
 			Vector3 pos = new Vector3 (x+ (i%3)*stepx, y + (i/3)*stepy);
 			GameObject obj = (GameObject) Instantiate(instance, pos, Quaternion.identity);
@@ -32,6 +46,26 @@ public class levelManager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void changePlant(int i){
+		if (i == 0) { //Left
+			if (plantSelected != 0) {
+				plantSelected--;
+				changeSprite ();
+			}
+		} else {
+			if (plantSelected != sprites.Length - 1) {
+				plantSelected++;
+				changeSprite ();
+			}
+		}
+	}
+
+	void changeSprite(){
+		//Animation para mudar a sprite?
+
+
 	}
 
 	void transicao(){
