1a4dfb388438b001ffbfe10b62d7cd87073a5f63
hmanfroi <henriquemanfroi@MacBook-Pro-de-Henrique.local>
2015-12-12 22:37:55 -0200

    LevelSelect

---
+++ Assets/Scripts/buttonLevel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonLevel : MonoBehaviour {
+
+	public int value;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void onClick(){
+		
+	}
+}
---
+++ Assets/Scripts/levelManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour {
+
+	public Button[] levelButtons;
+
+	public int totalLevels;
+	public int level;
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < levelButtons.Length; i++) {
+			int num = i;
+			levelButtons [i].GetComponentInChildren<Text> ().text = " "+(i+1);
+			levelButtons [i].onClick.AddListener (delegate {
+				level = num;
+				//
+				transicao();
+			});
+		}
+		/*for (int i = 0; i < levels; i++) {
+			Vector3 pos = new Vector3 (x+ (i%3)*stepx, y + (i/3)*stepy);
+			GameObject obj = (GameObject) Instantiate(instance, pos, Quaternion.identity);
+			levelButtons [i] = obj;
+		}
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void transicao(){
+		//Debug.Log ("Level: " + level);
+		//SceneManager.LoadScene ("level" + level.ToString ("d2"));
+		//
+	}
+
+}
